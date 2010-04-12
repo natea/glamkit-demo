67390e82e492a996faa53a0ac5c261a5e816d8ef
Greg Turner <assembla@gregturner.com>
2010-04-12 23:19:06 +1000

    hooray, the admin views appear to be generic

--- example_project/urls.py
+++ example_project/urls.py
@@ -6,13 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    (r'^$', direct_to_template, {'template': 'test.html'}),
-    (r'^blog/', include('example_project.blog.urls')),
+    url(r'^$', direct_to_template, {'template': 'test.html'}),
+    url(r'^blog/', include('example_project.blog.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
