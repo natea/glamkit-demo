f134a5e54a182cd45972457af4d12f5893721804
Greg Turner <assembla@gregturner.com>
2010-04-14 05:44:50 -0600

    event occurrences can now be hooked up to eventvariations

--- example_project/events/models.py
+++ example_project/events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from eventtools.models import EventBase, Rule, OccurrenceGeneratorModelBase, OccurrenceBase
+from eventtools.models import EventBase, EventVariationBase
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 
@@ -7,16 +7,16 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 class Event(EventBase):
     title = models.CharField(_("Title"), max_length = 255)
     location = models.CharField(_("Location"), max_length = 255, blank=True)
-    description = models.TextField(_("Description"), max_length=255, blank=True)
-    
+    description = models.TextField(_("Description"), max_length=255, blank=True)   
 
-    # varied_by = "EventVariation"
+    varied_by = "EventVariation"
     
     def __unicode__(self):
         return self.title
     
-# class EventVariation(models.Model):
-#     location = models.CharField(_("Location"), max_length = 255, blank=True, null=True)
-#     description = models.CharField(_("Plain English description of schedule"), max_length=255, blank=True, null=True)
-#     
-#     
+class EventVariation(EventVariationBase):
+    varies = "Event"
+    location = models.CharField(_("Alternative Location"), max_length = 255, blank=True, null=True)
+    description = models.TextField(_("Alternative Description"), max_length=255, blank=True, null=True)
+    
+    
