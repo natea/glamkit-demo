2774b405420f9203247d94561a5ae091fd1b5b99
Greg Turner <assembla@gregturner.com>
2010-06-29 16:58:28 +1000

    little bit of docs

--- example_project/events/models.py
+++ example_project/events/models.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 
 class Event(EventBase):
     title = models.CharField(_("Title"), max_length = 255)
+    slug = models.SlugField(_("Slug"), max_length = 255)
     location = models.CharField(_("Location"), max_length = 255, blank=True)
     description = models.TextField(_("Description"), max_length=255, blank=True)   
 
